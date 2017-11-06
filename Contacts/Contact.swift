1e75c0976250617f2fcd2d78a8935d7b2d2188e7
Carol Greenberg <look@theuxincubator.com>
2017-11-06 15:41:01 -0800

    Add files via upload

---
+++ Contacts/Contact.swift
@@ -0,0 +1,21 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Carol Greenberg on 11/5/17.
+//  Copyright © 2017 Carol Greenberg. All rights reserved.
+//
+
+import UIKit
+
+class Contact: NSObject {
+    var name = String?.init(nilLiteral: ())
+    var phoneNumber = String?.init(nilLiteral: ())
+    
+    init(name: String?, phoneNumber: String?) {
+        self.name = name
+        self.phoneNumber = phoneNumber
+        
+        super.init()
+    }
+}
